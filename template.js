250c78eb5b5e7aa4cbc920c11952d1c6a031c979
Constantin Tsukanov <ya-kostik@users.noreply.github.com>
2017-05-16 08:31:14 +0300

    Update template.js

--- template.js
+++ template.js
@@ -4,20 +4,36 @@ const zip = require('zip-stream');
 
 function docxTemplate(props, docx) {
   const archive = new zip({ zlib: { level: 0 } });
+  let promise = Promise.resolve();
+  function addFile(archive, stream, fileName) {
+    return new Promise((resolve, reject) => {
+      setImmediate(() => {
+        archive.entry(stream, { name: fileName }, function(err) {
+          if (err) return reject(err);
+          process.nextTick(() => resolve());
+        });
+      });
+    });
+  }
   docx.pipe(unzip.Parse()).
   on('entry', function (entry) {
     var fileName = entry.path;
     if (fileName === "word/document.xml") {
       const stream = entry.pipe(new Template(props));
-      process.nextTick(() => archive.entry(stream, { name: fileName }));
+      promise = promise.then(() => addFile(archive, stream, fileName));
     } else {
       if (fileName === '[Content_Types].xml') {
-        process.nextTick(() => {
-          archive.entry(entry, { name: fileName })
-          archive.finalize();
+        setImmediate(() => {
+          promise = promise.then(() => {
+            return addFile(archive, entry, fileName);
+          }).
+          then(() => archive.finalize());
+          // archive.entry(entry, { name: fileName })
+          // archive.finalize();
         });
       } else {
-        process.nextTick(() => archive.entry(entry, { name: fileName }));
+        promise = promise.then(() => addFile(archive, entry, fileName));
+        // setImmediate(() => archive.entry(entry, { name: fileName }));
       }
     }
   });
